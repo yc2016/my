cec7f4ba9eb41ffa073b39fce6b99aae924c3e2b
root <root@iZ25zhxvxduZ.(none)>
2016-02-14 17:18:15 +0800

    update

---
+++ thinkphp_5.0/application/runtime/template/541b2d3eaf84fa04d9f467aa08066372.php
@@ -0,0 +1,3 @@
+<?php if (!defined('THINK_PATH')) exit();echo $name; ?>
+<br>
+<?php echo U("Index/index"); ?>
---
+++ thinkphp_5.0/application/runtime/template/930f0a6ed6d723a3e6d4aaabd4e22a43.php
@@ -0,0 +1,1 @@
+<?php if (!defined('THINK_PATH')) exit();echo $name; echo U("Index/index"); ?>
